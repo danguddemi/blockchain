56ad4c63741b42afac0c696ae43bc43ebf521548
Daniel Guddemi <danguddemi@gmail.com>
2019-11-02 21:58:28 -0700

    Utilizing a merkle tree

--- src/utils.go
+++ src/utils.go
@@ -6,7 +6,7 @@ import (
 	"log"
 )
 
-// Converts an int64 to a byte array
+// IntToHex converts an int64 to a byte array
 func IntToHex(num int64) []byte {
 	buff := new(bytes.Buffer)
 	err := binary.Write(buff, binary.BigEndian, num)
@@ -17,7 +17,7 @@ func IntToHex(num int64) []byte {
 	return buff.Bytes()
 }
 
-// Reverses a byte array
+// ReverseBytes reverses a byte array
 func ReverseBytes(data []byte) {
 	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
 		data[i], data[j] = data[j], data[i]
